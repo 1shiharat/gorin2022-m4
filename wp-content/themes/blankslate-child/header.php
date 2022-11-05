b0ca9bf0e74c70d6286c0a31cad59b0101632093
1shiharaT <ishihara@grow-group.jp>
2022-11-05 05:20:10 +0000

    titleタグはプラグインで出力するので削除

--- wp-content/themes/blankslate-child/header.php
+++ wp-content/themes/blankslate-child/header.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head() ?>
-  <title>Making show</title>
+
 </head>
 
 <body <?php body_class() ?>>
